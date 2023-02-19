148296796d1d182da9dccf4919d14ca039250ec2
Seydali <lydesi24@gmail.com>
2023-02-19 16:02:25 +0300

    deploy config changed

--- astro.config.ts
+++ astro.config.ts
@@ -7,8 +7,7 @@ import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://link.com/",
-  outDir: 'public',
+	site: "https://lydesi.github.io/",
 	markdown: {
 		shikiConfig: {
 			theme: "dracula",
